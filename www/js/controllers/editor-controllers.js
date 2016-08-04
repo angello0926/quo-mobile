874dd549999167f09f287d1ec02ced02d7c76230
Angel Lo <angelyukyu@gmail.com>
2016-08-04 14:36:27 +0800

    text and color done

--- www/js/controllers/editor-controllers.js
+++ www/js/controllers/editor-controllers.js
@@ -1,42 +1,61 @@
 angular.module('quo')
 
 .controller('editorController', function($scope,$state,$ionicActionSheet) {
+
+  console.log($scope)
   $scope.fonts=["Merriweather", "Inconsolata", "Abel", "Quicksand", "Playfair Display", "Roboto Condensed"];
   $scope.colorsDefault=['black','white','#607d8b','#555555','#ffc93b'];
-  $scope.colorsBgDefault=['#020101','#FFFFFF','#555555','#31302C','#FCEE21','#0B2B62'];
+  $scope.colorsBgDefault=['#e74c3c','#e67e22','#f1c40f','#1abc9c','#34495e','#ecf0f1'];
   $scope.textParams   = {
     fontSelected: 'Merriweather',
     colorSelected: 'black',
     textContent:''
   };
-  $scope.colorParams  = {bgColor: 'white'};
+  $scope.colorParams  = {bgColor: 'white', fillColor: 'white', borderColor:'white'};
   $scope.shapesParams = {};
   $scope.photoParams  = {};
 
-  $('#container').width(screen.width-2);
-  var width=screen.width-2;
-  console.log(width);
-  var stage = new Konva.Stage({
-    container: 'container',
-    name: "canvas",
-    width: width,
-    height: width*0.75
-  });
+  $scope.initiateStage=function(){
+    var windowHeight = $(document).height();
+    var navbarHeight = $('.editor-navbar').outerHeight();
+    var canvasWidth  = screen.width - 2;
+    var canvasHeight = canvasWidth * 0.75;
 
-  var layer = new Konva.Layer();
-  var bg = new Konva.Rect({
-    x: 0,
-    y: 0,
-    fill: '#F0F0F0',
-    width: width,
-    height: width*0.75,
-    id:"bg"
-  });
-  layer.add(bg);
-  stage.add(layer);
+    $scope.tabStyle = {
+      height: windowHeight - navbarHeight - canvasHeight
+    }
+
+    console.log($scope.tabStyle)
+    $scope.canvasStyle = {
+      width: canvasWidth,
+      height: canvasHeight
+    }
+
+    $scope.stage = new Konva.Stage({
+      container: 'canvas',
+      name: "canvas",
+      width: canvasWidth,
+      height: canvasHeight
+    })
+
+    $scope.layer = new Konva.Layer();
+
+    var bg = new Konva.Rect({
+      x: 0,
+      y: 0,
+      fill: '#F0F0F0',
+      width: canvasWidth,
+      height: canvasHeight,
+      id:"bg"
+    });
+
+    $scope.layer.add(bg);
+    $scope.stage.add($scope.layer);
+  }
+  $scope.initiateStage();
 
   $scope.saveImage = function(){
-    var dataURL = stage.toDataURL();
+    var dataURL = $scope.stage.toDataURL();
     window.open(dataURL);
   }
 
@@ -44,7 +63,7 @@ angular.module('quo')
     $('.normal').removeClass('').addClass('ion-record normal');
     $scope.textParams.colorSelected= $scope.colorsDefault[index];
     console.log($scope.textParams.colorSelected);
-    $('#'+index).removeClass('ion-record').addClass('ion-checkmark-circled');
+    $('.normal'+'#'+index).removeClass('ion-record').addClass('ion-checkmark-circled');
   }
   $scope.sendTexttoCanvas = function (){
      var layer=new Konva.Layer();
@@ -94,7 +113,7 @@ angular.module('quo')
       this.moveToTop();
       layer.draw();
     })
-    stage.add(layer);
+    $scope.stage.add(layer);
     $('textarea').val('');
    }
 
@@ -112,22 +131,22 @@ angular.module('quo')
 
     // update anchor positions
     switch (activeAnchor.getName()) {
-        case 'topLeft':
-            topRight.setY(anchorY);
-            bottomLeft.setX(anchorX);
-            break;
-        case 'topRight':
-            topLeft.setY(anchorY);
-            bottomRight.setX(anchorX);
-            break;
-        case 'bottomRight':
-            bottomLeft.setY(anchorY);
-            topRight.setX(anchorX);
-            break;
-        case 'bottomLeft':
-            bottomRight.setY(anchorY);
-            topLeft.setX(anchorX);
-            break;
+      case 'topLeft':
+          topRight.setY(anchorY);
+          bottomLeft.setX(anchorX);
+          break;
+      case 'topRight':
+          topLeft.setY(anchorY);
+          bottomRight.setX(anchorX);
+          break;
+      case 'bottomRight':
+          bottomLeft.setY(anchorY);
+          topRight.setX(anchorX);
+          break;
+      case 'bottomLeft':
+          bottomRight.setY(anchorY);
+          topLeft.setX(anchorX);
+          break;
     }
 
     text.position(topLeft.position());
@@ -135,16 +154,15 @@ angular.module('quo')
     var width = topRight.getX() - topLeft.getX();
     var height = bottomLeft.getY() - topLeft.getY();
     if(width && height) {
-        text.fontSize((width/10));
-        text.height(height);
-        text.width(width);
+      text.fontSize((width/10));
+      text.height(height);
+      text.width(width);
     }
   }
 
   $scope.addAnchor=function(group,x,y,name){
     var stage = group.getStage();
     var layer = group.getLayer();
-
     var anchor = new Konva.Circle({
       x: x,
       y: y,
@@ -156,42 +174,58 @@ angular.module('quo')
       dragOnTop: false
     });
     anchor.on('dragmove', function() {
-        $scope.updateTextSize(this);
-        layer.draw();
+      $scope.updateTextSize(this);
+      layer.draw();
     });
     anchor.on('mousedown touchstart', function() {
-        group.setDraggable(false);
-        this.moveToTop();
+      group.setDraggable(false);
+      this.moveToTop();
     });
     anchor.on('dragend', function() {
-        group.setDraggable(true);
-        layer.draw();
+      group.setDraggable(true);
+      layer.draw();
     });
     // add hover styling
     anchor.on('mouseover', function() {
-        var layer = this.getLayer();
-        document.body.style.cursor = 'pointer';
-        this.setStrokeWidth(4);
-        layer.draw();
+      var layer = this.getLayer();
+      document.body.style.cursor = 'pointer';
+      this.setStrokeWidth(4);
+      layer.draw();
     });
     anchor.on('mouseout', function() {
-        var layer = this.getLayer();
-        document.body.style.cursor = 'default';
-        this.setStrokeWidth(2);
-        layer.draw();
+      var layer = this.getLayer();
+      document.body.style.cursor = 'default';
+      this.setStrokeWidth(2);
+      layer.draw();
     });
 
     group.add(anchor);
   }
 
   $scope.BgColorChosen= function(index){
-     $('.normalbg').removeClass('').addClass('ion-record normal');
+    $('.normalbg').removeClass('').addClass('ion-record normal');
     $scope.colorParams.bgColor= $scope.colorsBgDefault[index];
     console.log($scope.colorParams.bgColor);
-    $('.normalbg'+'#'+index).removeClass('ion-record').addClass('ion-checkmark-circled');
-    var shape = stage.find('#bg')[0];
+    $('#'+'color'+index).removeClass('ion-record').addClass('ion-checkmark-circled');
+    var shape = $scope.stage.find('#bg')[0];
     shape.fill($scope.colorsBgDefault[index]);
-    layer.draw();
+    $scope.layer.draw();
   }
 
+  $scope.fillColorChosen=function(index){
+    $('.normalfill').removeClass('').addClass('ion-record normal');
+    $scope.colorParams.fillColor= $scope.colorsBgDefault[index];
+    console.log($scope.colorParams.fillColor);
+    $('#'+'fillColor'+index).removeClass('ion-record').addClass('ion-checkmark-circled');
+
+  }
+
+   $scope.borderColorChosen=function(index){
+    $('.normalborder').removeClass('').addClass('ion-record normal');
+    $scope.colorParams.borderColor= $scope.colorsBgDefault[index];
+    console.log($scope.colorParams.fillColor);
+    $('#'+'borderColor'+index).removeClass('ion-record').addClass('ion-checkmark-circled');
+
+  }
 })
+
